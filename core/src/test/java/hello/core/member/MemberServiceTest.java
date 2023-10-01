a63e971694f2529cdecaa0a77861f56ed5c88a0d
sangkeonLee <leesk928@gmail.com>
2023-10-01 17:39:46 +0900

    스프링 핵심 원리 - 기본편  > 섹션 3. 스프링 핵심 원리 이해2 - 객체 지향 원리 적용 -- 할인 적용 Rate로 변경하여 개발 및 테스트 -- Appconfig.java 생성하여 외부에서 생성자를 생성하여 전달(DI, 역할과 구현을 분리함) -- Appconfig.java Spring 형태로 변경

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,23 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+//        AppConfig appConfig = new AppConfig();
+//        memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        memberService = applicationContext.getBean("memberService", MemberService.class);
+    }
 
     @Test
     void join() {
